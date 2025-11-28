4bba629ace58e4747c86132120fca9ae73615006
dr4k0b <jakob.liljenstrom@gmail.com>
2025-11-28 15:10:20 +0100

    Clementine + intermission polish

--- Christmasware/Assets/Scripts/Clementine.cs
+++ Christmasware/Assets/Scripts/Clementine.cs
@@ -1,16 +1,57 @@
+using System.Collections;
 using UnityEngine;
 
 public class Clementine : MiniGame
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    Animator aniClementine;
+    public Animator aniKey;
+    int clementine;
+    int key;
     void Start()
     {
-        
+        aniClementine = GetComponent<Animator>();
+        clementine = 0;
+        key = Random.Range(1, 5);
     }
-
-    // Update is called once per frame
     void Update()
     {
+        KeyCode keyToPress = KeyCode.Return;
         Timer();
+        aniKey.SetInteger("key", key);
+        if (key == 1)
+        {
+            keyToPress = KeyCode.W;
+        }
+        if (key == 2)
+        {
+            keyToPress = KeyCode.A;
+        }
+        if (key == 3)
+        {
+            keyToPress = KeyCode.S;
+        }
+        if (key == 4)
+        {
+            keyToPress = KeyCode.D;
+        }
+        if (key == 5)
+        {
+            keyToPress = KeyCode.Space;
+        }
+        if (Input.GetKeyUp(keyToPress))
+        {
+            clementine++;
+            aniClementine.SetInteger("Peel", clementine);
+            key = Random.Range(1, 5);
+        }
+        if (clementine == 3)
+        {
+            StartCoroutine(WinDelay());
+        }
+    }
+    IEnumerator WinDelay()
+    {
+        yield return new WaitForSeconds(0.25f);
+        Win();
     }
 }
--- Christmasware/Assets/Scripts/IntermissionLogic.cs
+++ Christmasware/Assets/Scripts/IntermissionLogic.cs
@@ -5,21 +5,48 @@ using UnityEngine.SceneManagement;
 public class IntermissionLogic : MonoBehaviour
 {
     GlobalInformation g;
+    public Animator ani;
     void Start()
     {
         g = FindAnyObjectByType<GlobalInformation>();
+        if (g.round == 0)
+        {
+            ani.SetBool("Game", false);
+        }
+        else if (g.result == GlobalInformation.Result.win)
+        {
+            ani.SetBool("Win", true);
+        }
+        else if (g.result == GlobalInformation.Result.lose)
+        {
+            ani.SetBool("Win", true);
+        }
         g.round++;
         if (g.round % 5 == 0)
         {
             g.Difficulty++;
         }
-
+        
         StartCoroutine(NextScene());
         // start new minigame
     }
 
     private void Update()
     {
+        if (g.result == GlobalInformation.Result.start)
+        {
+            ani.SetBool("Game", true);
+        }
+        else if (g.result == GlobalInformation.Result.win)
+        {
+            ani.SetBool("Win", true);
+            ani.SetBool("Game", false);
+        }
+        else if (g.result == GlobalInformation.Result.lose)
+        {
+            ani.SetBool("Win", false);
+            ani.SetBool("Game", false);
+        }
         if (Input.GetKey(KeyCode.D))
         {
             SceneManager.LoadScene("Present");
